b64e3f543e6c1db27d1d71c5b56560c880938bda
CryptoFixe <altieres.jesus@cryptofixe.com>
2023-09-29 20:05:04 +0100

    Production Version

--- Token/CryptoFixe.sol
+++ Token/CryptoFixe.sol
@@ -19,7 +19,7 @@ pragma solidity ^0.8.21;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
 
 contract CryptoFixe is ERC20, ERC20Burnable, Ownable {
 
